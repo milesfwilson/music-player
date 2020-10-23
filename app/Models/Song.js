71bda3e53de14fb1b95dd6eb3b6d586a4036f97d
milesfwilson <milesfwilson@gmail.com>
2020-10-22 21:28:15 -0600

    styled

--- app/Models/Song.js
+++ app/Models/Song.js
@@ -12,15 +12,22 @@ export default class Song {
 
   get Template() {
     return /*html*/`
-        <div class="row shadow-lg my-2"  data-toggle="modal" data-target="#song-${this._id}">
-          <div class="col-4 p-2">
+        <div class="row shadow bg-transparent my-2 radius bg-dark text-light"  data-toggle="modal" data-target="#song-${this._id}">
+          <div class="col-3 p-1">
             <img src="${this.albumArt}" class="img-fluid" alt=""/>
           </div>
-          <div class="col p-2">
-            <h6>Title: ${this.title}</h6>
-            <h6>Artist: ${this.artist}</h6>
-            <h6>Album: ${this.album}</h6>
+          <div class="col-1"></div>
+          <div class="col-7 p-1 d-flex">
+<div class="my-auto">
+<h6>${this.title}</h6>
+<h6>${this.artist}</h6>
+<h6>${this.album}</h6>
+</div>
           </div>
+<div class="col-1 p-1 d-flex justify-content-center">
+<i class="fa fa-play-circle my-auto fa-2x" aria-hidden="true"></i>
+</div>
+
         </div>
         
 
@@ -28,31 +35,32 @@ export default class Song {
         <!-- Modal -->
         <div class="modal fade" id="song-${this._id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-             
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="app.songsController.audioPause('${this._id}')">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
+          <div class="modal-content bg-dark text-white">
+
             <div class="modal-body row">
             <div class="col-5 p-2">
             <img src="${this.albumArt}" class="img-fluid" alt=""/>
           </div>
-          <div class="col p-2">
-          <h5>Title: ${this.title}</h5>
-          <h5>Artist: ${this.artist}</h5>
-          <h5>Album: ${this.album}</h5>
+          <div class="col-7 p-2 d-flex">
+<div class="my-auto">
+<h5>${this.title}</h5>
+<h5>${this.artist}</h5>
+<h5>${this.album}</h5>
+</div>
           </div>
           
-              <div class="col p-2">
-              ${this.audio}
-           </div>
+              
             </div>
             <div class="modal-footer">
+            ${this.audio}
+              <button type="button" class="btn bg-transparent text-light border-0" data-dismiss="modal" onclick="app.songsController.audioPause('${this._id}')">
+              
+              <i class="far fa-times-circle"></i>
+              </button>
+              <button type="button" class="btn bg-transparent text-light border-0" onclick="app.songsController.addSong('${this._id}')">
             
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary" onclick="app.songsController.addSong('${this._id}')">Purchase</button>
+            <i class="fa fa-plus-circle" aria-hidden="true"></i>
+              </button>
             </div>
           </div>
         </div>
@@ -61,21 +69,23 @@ export default class Song {
   }
   get playlistTemplate() {
     return /*html*/`
-            <div class="col-3 shadow-lg m-1">
-              <button onclick="app.songsController.removeSong('${this._id}')" class="close text-danger">x</button>
-              <img class="img-fluid pt-1" src="${this.albumArt}" onclick="app.songsController.playPlaylist('${this._id}')" alt=""/>
-              <p>${this.title}</p>
-              <p>${this.artist}</p>
-
-            </div>
+    <div class="col-4">
+    <div class="shadow bg-transparent radius text-light m-3 p-2 text-center">
+    <button onclick="app.songsController.removeSong('${this._id}')" class="close text-light">x</button>
+    <img class="img-fluid pt-1" src="${this.albumArt}" onclick="app.songsController.playPlaylist('${this._id}')" alt=""/>
+    <p class="mt-2">${this.title}</p>
+    <p>${this.artist}</p>
+    
+    </div>
+    </div>
         `;
   }
 
 
   get activeSongTemplate() {
     return /*html*/`
-      <div class="row">
-        <div class="col-2">
+      <div class="row bg-dark text-light p-1">
+        <div class="col-1">
         <img class="img-fluid" src="${this.albumArt}" alt=""/>
         </div>
 
